f33432954d08a5458329b36da3cf1c4879503c95
sumanitian <sumanprasad26042001@gmail.com>
2024-06-02 10:26:52 +0530

    installing

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # visa-approval
+
+## How to Execute Easily?
+
+```bash
+conda create -n visa python=3.8 -y
+```
+
+```bash
+conda activate visa
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="visa",
+    version="0.0.1",
+    author="updatesoon",
+    author_email="updatesoon@gmail.com",
+    packages=find_packages()
+)
